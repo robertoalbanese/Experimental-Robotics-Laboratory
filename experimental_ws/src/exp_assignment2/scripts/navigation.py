7dce5972313eb3ca5734d0f5ad760102ec7ebf01
Robbai <ralbanese18@gmail.com>
2020-12-15 10:32:31 +0100

    Edit from server to action server

--- experimental_ws/src/exp_assignment2/scripts/navigation.py
+++ experimental_ws/src/exp_assignment2/scripts/navigation.py
@@ -1,38 +1,68 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
+# import ros stuff
 import rospy
-from exp_assignment2.srv import ReachNextPosition
-from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist, Point
 from nav_msgs.msg import Odometry
+from gazebo_msgs.msg import LinkState
+from tf import transformations
+import time
+import math
+import actionlib
+import actionlib.msg
+import exp_assignment2.msg
+from exp_assignment2.srv import ReachNextPosition
+from turtlesim.msg import Pose
 from math import pow, atan2, sqrt
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 
-class Navigazion:
+class Navigation:
 
     def __init__(self):
-		# Creates a node with name 'navigation'.
-		rospy.init_node('navigation')
+        # Creates a node with name 'navigation'.
+        rospy.init_node('navigation')
+        # Current position
+        self.msg_pose = Pose()
 
-		# Publisher which will publish to the topic '/turtle1/cmd_vel'.
-		self.velocity_publisher = rospy.Publisher('/robot/cmd_vel', Twist, queue_size=10)
+        self.rate = rospy.Rate(20)
+        # distance tolerance for navigation
+        self.distance_tolerance = 0.1
+        # machine state
+        self.state = 0
 
-		# A subscriber to the topic '/turtle1/pose'. self.update_pose is called
-		# when a message of type Pose is received.
-		self.pose_subscriber = rospy.Subscriber('/robot/odom', Odometry, self.update_pose)
-		
-		# A service for the server /robot/reach_new_position'. move2goal() is called
-        # whenever a cliend request is received. 
-		self.nav_server = rospy.Service('/robot/reach_new_position', ReachNextPosition, self.move2goal)
+        # Publisher which will publish to the topic '/robot/cmd_vel'.
+        self.velocity_publisher = rospy.Publisher(
+            '/robot/cmd_vel', Twist, queue_size=10)
 
-		self.msg_pose = Pose()
-		self.rate = rospy.Rate(10)
-        
+        # A subscriber to the topic '/robot/pose'. self.update_pose is called
+        # when a message of type Pose is received.
+        self.msg_posesubscriber = rospy.Subscriber(
+            '/robot/odom', Odometry, self.update_pose)
+
+        # An action service for the server /robot/reach_new_position'. move2goal() is called
+        # whenever a cliend request is received.
+        self.act_s = actionlib.SimpleActionServer(
+            '/robot/reaching_goal', exp_assignment2.msg.PlanningAction, self.planning, auto_start=False)
+        self.act_s.start()
+
+    def change_state(self, new_state):
+        self.state = new_state
+        print ('State changed to [%s]' % self.state)
+
     def update_pose(self, data):
         """Callback function which is called when a new message of type Pose is
         received by the subscriber."""
 
+        # Get yaw from quaternions
+        orientation_q = data.pose.pose.orientation
+        orientation_list = [orientation_q.x,
+                            orientation_q.y, orientation_q.z, orientation_q.w]
+        (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
         self.msg_pose.x = round(data.pose.pose.position.x, 4)
         self.msg_pose.y = round(data.pose.pose.position.y, 4)
+        self.msg_pose.theta = round(yaw, 4)
+        print self.msg_pose
 
     def euclidean_distance(self, goal_pose):
         """Euclidean distance between current pose and the goal."""
@@ -48,21 +78,18 @@ class Navigazion:
     def angular_vel(self, goal_pose, constant=2):
         return constant * (self.steering_angle(goal_pose) - self.msg_pose.theta)
 
-    def move2goal(self,data):
-        """Moves the turtle to the goal."""
+    def move2goal(self, data):
+        """Moves the robot to the goal."""
         goal_pose = Pose()
 
         # Get the input from the user.
-        goal_pose.x = data.x
-        goal_pose.y = data.y
-        print goal_pose
+        goal_pose.x = data.target_pose.pose.position.x
+        goal_pose.y = data.target_pose.pose.position.y
 
-        # Please, insert a number slightly greater than 0 (e.g. 0.01).
-        distance_tolerance = 0.01
-
+        err_pos = self.euclidean_distance(goal_pose)
         vel_msg = Twist()
 
-        while self.euclidean_distance(goal_pose) >= distance_tolerance:
+        if err_pos >= self.distance_tolerance:
 
             # Porportional controller.
 
@@ -75,25 +102,64 @@ class Navigazion:
             vel_msg.angular.x = 0
             vel_msg.angular.y = 0
             vel_msg.angular.z = self.angular_vel(goal_pose)
-            
+
             # Publishing our vel_msg
             self.velocity_publisher.publish(vel_msg)
 
             # Publish at the desired rate.
             self.rate.sleep()
 
-        # Stopping our robot after the movement is over.
-        vel_msg.linear.x = 0
-        vel_msg.angular.z = 0
-        self.velocity_publisher.publish(vel_msg)
-        
+        else:
+            print ('Position error: [%s]' % err_pos)
+            self.change_state(1)
+
+    def done(self):
+        twist_msg = Twist()
+        twist_msg.linear.x = 0
+        twist_msg.linear.y = 0
+        pub.publish(twist_msg)
+
+    def planning(self, goal):
+
+        self.state = 0
+        success = True
+        print goal
+        feedback = exp_assignment2.msg.PlanningFeedback()
+        result = exp_assignment2.msg.PlanningResult()
+
+        while not rospy.is_shutdown():
+            if self.act_s.is_preempt_requested():
+                rospy.loginfo('Goal was preempted')
+                self.act_s.set_preempted()
+                success = False
+                break
+            elif self.state == 0:
+                feedback.stat = "Reaching the goal"
+                feedback.position = self.msg_pose
+                self.act_s.publish_feedback(feedback)
+                self.move2goal(goal)
+            elif self.state == 1:
+                feedback.stat = "Target reached!"
+                feedback.position = self.msg_pose
+                self.act_s.publish_feedback(feedback)
+                self.done()
+                break
+            else:
+                rospy.logerr('Unknown state!')
+
+        # If we press control + C, the node will stop.
+        rospy.spin()
+        if success:
+            rospy.loginfo('Goal: Succeeded!')
+            self.act_s.set_succeeded(result)
+
 
 if __name__ == '__main__':
     try:
-        x = Navigazion()
-        
-		# If we press control + C, the node will stop.
+        x = Navigation()
+
+        # If we press control + C, the node will stop.
         rospy.spin()
-        
+
     except rospy.ROSInterruptException:
         pass
